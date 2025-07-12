3a903da758eefdeb4e876db8f1dfb0c86876e322
hyeonseo1109 <alsgustj2@naver.com>
2025-07-12 17:28:39 +0900

    입력값 json서버에 연동

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect, useRef } from 'react'
+import './App.css'
+
+function App() {
+  const [list, setList] = useState([]);
+  const [data] = useFetch("http://localhost:3000/content");
+
+  useEffect( () => {
+    if (data) {
+      setList(data);
+    }
+  }, [data])
+  
+
+  return (
+    <>
+      {list.map((el => <div className='border m-[10px]'>{el.text}</div>))}
+      <ListInput setList={setList}/>
+    </>
+  )
+}
+
+const ListInput = ({setList}) => {
+  const inputRef = useRef(null);
+
+  const addList = () => {
+    const newList = {
+      text: inputRef.current.value,
+    };
+    fetch("http://localhost:3000/content", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newList),
+    })
+    .then((res) => (res.json()))
+    .then((res) => setList((prev) => [...prev, res]))
+    inputRef.current.value = ''
+  };
+  return (
+    <div>
+      <input ref={inputRef} placeholder='ex: 120/80' className='border'/>
+      <button onClick={addList}>+</button>
+    </div>
+  )
+}
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect( () => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((res)=> {
+      setData(res);
+    });
+  }, [url]);
+  return [data]
+}
+export default App
