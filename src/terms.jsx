7d99e097f926204bcd983bd433be88315c92b72a
hyeonseo1109 <alsgustj2@naver.com>
2025-07-16 22:01:31 +0900

    수정: mongoDB URI 변경, gitignore 재작성
    -mongoDB URI를 변경하여 .env 내에서도 수정하였음.
    -gitignore에서  env의 파일 경로 오류 발견, 수정하였음.

--- src/terms.jsx
+++ src/terms.jsx
@@ -1,6 +1,6 @@
 export const Terms = () => (
     <div>
-        <h3 className="text-red-600 text-[15px] whitespace-nowrap">해당 내용은 기록 및 참고용이며, 전문적인 의학적 진단을 대체하지 않습니다.</h3>
-        <p className="text-[15px] whitespace-nowrap">입력하신 정보는 오로지 개인의 건강 기록을 위한 목적으로만 사용됩니다. </p>
+        <h3 className="text-red-600 text-[15px]">해당 내용은 기록 및 참고용이며, 전문적인 의학적 진단을 대체하지 않습니다.</h3>
+        <p className="text-[15px]">입력하신 정보는 오로지 개인의 건강 기록을 위한 목적으로만 사용됩니다. </p>
     </div>
 )
